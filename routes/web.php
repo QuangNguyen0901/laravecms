a1f6c0323e44782c6897f4c9cc260a0fb4f1eeda
QuangNguyen <hikari0901@gmail.com>
2018-07-30 19:04:38 +0700

    db

--- routes/web.php
+++ routes/web.php
@@ -25,4 +25,26 @@ Route::get('where',function (){
 
 Route::get('profile/{name}','ProfileController@showProfile');
 
+Route::get('insert',function (){
+   DB::insert('insert into users(name,email,password) VALUES (?,?,?)',['vana','hikari0901@gmail.com','123456']);
+   return 'insert successful';
+});
+
+Route::get('read',function(){
+    $result = DB::select('select * from users where id = ? ',[1]);
+    foreach ($result as $user ){
+        return $user->name;
+    }
+});
+
+Route::get ('update',function (){
+    $updated = DB::update('update users set name = ? where id = ?',['ahaha',1]);
+    return $updated;
+});
+
+Route::get('delete',function (){
+    $deleted = DB::delete('delete from users where id = ?',[2]);
+    return $deleted;
+});
+
 
